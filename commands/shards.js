b2b28456ba3ad4a582bf0d562db739523c8918fa
Solace <treynolds4@student.yosemite.edu>
2017-10-16 10:35:07 -0700

    Added :t reply | improved :t shards

--- commands/shards.js
+++ commands/shards.js
@@ -3,8 +3,9 @@ module.exports = {
   execute:async function(bot, msg, args){
     await msg.channel.createMessage("Getting Shards...")
     .then((message)=>{
-      let shardMap = bot.shards.map(s => `[ID]: ${s.id} | [Ping]: ${s.latency} | [Status]: ${s.status}`).join("\n");
-      message.edit("**Shards:**\n```INI\n"+shardMap+"```");
+      let shardMap = bot.shards.map(s => `= [ID]: ${s.id} | [Ping]: ${s.latency} | [Status]: ${s.status} =`).join("\n");
+      let s = msg.channel.guild.shard;
+      message.edit(`\`\`\`asciidoc\n[Current Shard]\n= [ID]: ${s.id} | [Ping]: ${s.latency} | [Status]: ${s.status} =\n[All Shards]\n${shardMap}\n\`\`\``);
     });
   }
 }
