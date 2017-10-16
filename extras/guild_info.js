84b7e46b22c73e626c6d9e3adb1658e3bb02f37a
Solace <treynolds4@student.yosemite.edu>
2017-10-16 14:05:11 -0700

    fixed #join-log guild logging

--- extras/guild_info.js
+++ extras/guild_info.js
@@ -3,7 +3,7 @@ module.exports = {
   update:async function(bot, guild, type){
     var guild_state = (type ? "New Guild Joined" : "Guild Left");
     if (!guild.iconURL){
-      bot.createMessage(config.guild_state_channel, { embed: {
+      bot.createMessage(config.guild_stat_channel, { embed: {
         color: 0xFFFFFF, description: `${guild_state}, now at ${bot.guilds.size} guilds!`,
         fields: [
           { name: 'GuildID', value: `${guild.id}` },
@@ -16,7 +16,7 @@ module.exports = {
         thumbnail: { url: `http://is2.mzstatic.com/image/thumb/Purple128/v4/b1/9b/6c/b19b6c17-4e81-d800-3d1e-c0935f5ec5ba/source/300x300bb.jpg` }
       }});
     }else{
-      bot.createMessage(config.guild_state_channel, { embed: {
+      bot.createMessage(config.guild_stat_channel, { embed: {
         color: 0xFFFFFF, description: `${guild_state}, now at ${bot.guilds.size} guilds!`,
         fields: [
           { name: 'GuildID', value: `${guild.id}` },
