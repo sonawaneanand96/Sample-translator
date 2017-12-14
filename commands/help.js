b8b8ad0f15dc5e4cf93971686a9ef8c4d7fb1168
Solace <treynolds4@student.yosemite.edu>
2017-12-14 04:32:33 -0800

    updated

--- commands/help.js
+++ commands/help.js
@@ -1,7 +1,7 @@
 module.exports = {
   command:"help",
   execute:async function(bot, msg, args){
-    await msg.channel.createMessage({embed: {
+    msg.channel.createMessage({embed: {
       color: 0xFFFFFF,
       author: {
         name: 'Learning How To Use Your New Translator',
@@ -17,17 +17,22 @@ module.exports = {
           value: 'If you see people chatting away in a language you dont know, and you want to take part in the conversaion, you can type **":t lang (text to analyze)"** and Translate will dissect the message and tell you what language they\'re speaking in! For example, All I\'d have to type is **":t lang 안녕하세요, 만나서 반가워요!"** to find out that they\'re speaking in korean'
         },
         {
+          name: 'Translating Messages Automatically',
+          value: 'We now allow automatic translations! To set up a channel to allow automatic translations between two languages, simply run ":t channel (first language iso) (second language iso)". This will translate all text to those the two selected languages! **Full list of language isos:** http://kpop4.us/s/molcoz'
+        },
+        {
           name: 'Other commands available',
           value: '```ini\n[:t patreon] Gives you a link to our patreon!\n\n[:t invite] Sends an invite link so that your friends can invite Translate to their servers too\n\n[:t bug (bug report)] Sends a bug report to the developers\n\n[:t suggest (suggestion)] Sends a suggestion to our developers\n\n[:t stats] Shows some cool technical statistic nerd-stuff about the bot\n\n[:t shards] Displays all the bot\'s shards, along with their pings\n```'
         },
         {
           name: 'More Information',
-          value: 'Want to learn more about Translate? **Want to see the full list of supported languages?** Want to upvote Translate? You can learn more about Translate and can upvote the bot here: <https://discordbots.org/bot/translate>. We appreciate any and all upvotes, and *thank you* for supporting our growth'
+          value: 'Want to learn more about Translate? **Want to see the full list of supported languages?** Want to upvote Translate? You can learn more about Translate here: <http://translatebot.xyz/translate/help>.'
         }
         ],
         thumbnail: {
           url: `http://ovh-is.a-shit.host/87440.png`
         }
     }});
+    msg.channel.createMessage('Do you want to contribute to the development of this bot? Want to see updates and our other services before anybody else? Are you a translator? Then join our development server to help us continue translating! https://discord.gg/J2E8GRM')
   }
 }
