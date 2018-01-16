2d89a46aa805ef1860c2d8dafd0b81e5c31f38be
Solace <treynolds4@student.yosemite.edu>
2018-01-16 13:48:39 -0800

    removed discordbots.org from send_stats

--- extras/send_stats.js
+++ extras/send_stats.js
@@ -1,18 +1,8 @@
-const request = require("superagent"), config = require('./../tlcfg.json'), dbotspw_token = config.dbotspw, dbots_token = config.dbots;
+const request = require("superagent"), config = require('./../tlcfg.json'), dbotspw_token = config.dbotspw;
 module.exports = {
   send:async function(guild_size, shard_size){
     if(guild_size == null) return;
     if(shard_size == null) return;
-    request.post(`https://discordbots.org/api/bots/318554710929833986/stats`)
-    .set('Authorization', dbots_token)
-    .send({ server_count: guild_size, shard_count: shard_size })
-    .end(function(err, res){
-      if(err){
-        return console.log(`ERROR SENDING STATS TO DISCORDBOTS.ORG: ${err}`);
-      }else{
-        console.log(`Successfully sent stats to https://discordbots.org! Now at ${guild_size} servers!`);
-      }
-    });
     request.post(`https://bots.discord.pw/api/bots/318554710929833986/stats`)
     .set('Authorization', dbotspw_token)
     .send({ server_count: guild_size })
