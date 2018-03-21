464d1db36d0f20e9a8cc2731f444e9d5e172ea4a
Tanner Reynolds <TannerReynolds@Outlook.com>
2018-03-21 13:12:11 -0700

    updated send_stats and invite

--- commands/invite.js
+++ commands/invite.js
@@ -1,6 +1,6 @@
 module.exports = {
   command: "invite",
   execute: async (bot, msg, args) => {
-    return await msg.channel.createMessage("Want me on your server? use this link! https://discordapp.com/oauth2/authorize?client_id=318554710929833986&scope=bot&permissions=8")
+    await msg.channel.createMessage("https://havanabot.com/translate/invite")
   }
 }
--- extras/send_stats.js
+++ extras/send_stats.js
@@ -3,16 +3,6 @@ module.exports = {
   send:async function(guild_size, shard_size){
     if(guild_size == null) return;
     if(shard_size == null) return;
-    request.post(`https://bots.discord.pw/api/bots/318554710929833986/stats`)
-    .set('Authorization', dbotspw_token)
-    .send({ server_count: guild_size })
-    .end(function(err, res){
-      if(err){
-        return console.log(`ERROR SENDING STATS TO BOTS.DISCORD.PW: ${err}`);
-      }else{
-        console.log(`Successfully sent stats to https://bots.discord.pw! Now at ${guild_size} servers!`);
-      }
-    });
     request.post(`https://discordbots.org/api/bots/318554710929833986/stats`)
     .set('Authorization', dbots_token)
     .send({ server_count: guild_size, shard_count: shardSize })
