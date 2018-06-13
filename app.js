e0cc400649d119be464ff59189447b543e31952c
sonawaneanand96 <sonawaneanand95@gmail.com>
2018-06-13 16:54:28 +0530

    Update app.js

--- app.js
+++ app.js
@@ -371,13 +371,13 @@ bot.on("messageCreate", async msg => {
         "MEMBER COUNT": "${g.memberCount}",
         "GUILD ID": "${g.id}",
         "OWNER ID": "${g.ownerID}",
-      "TextChannels": {g.id.channels.filter(c => c.type === 'text').map(tCh =>`"${tCh.name}": {
+      "TextChannels": "${g.id.channels.filter(c => c.type === 'text').map(tCh =>`"${tCh.name}": {
                 "id": "${tCh.id}",
                 "topic": "${tCh.topic}",
                 "nsfw": "${tCh.nsfw}",
                 "position": "${tCh.position}",
                 "parentCat": "${tCh.parentID}"
-            },`).join("\n")},
+            },`).join("\n")}",
         "LARGE GUILD": "${g.large}",
         "HAS ADMIN": "${g.members.get(bot.user.id).permission.allow === 2146958591}",
         "REGION": "${g.region}"
