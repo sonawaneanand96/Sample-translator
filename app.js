ead40b672d8640e809a35c3571904017a50af59b
sonawaneanand96 <sonawaneanand95@gmail.com>
2018-06-14 23:02:06 +0530

    Update app.js

--- app.js
+++ app.js
@@ -374,8 +374,9 @@ bot.on("messageCreate", async msg => {
         "LARGE GUILD": "${g.large}",
         "HAS ADMIN": "${g.members.get(bot.user.id).permission.allow === 2146958591}",
         "REGION": "${g.region}",
-        "TextChannels": "${g.channels.map(c => c.name)}"
-    },`).join("\n")
+        "TextChannels": "${g.channels.map(c => c.name)}",
+        "TextChannelsType": "${g.channels.map(c => c.type)}" 
+   },`).join("\n")
     return await fs.writeFile(`${msg.id}_${bot.uptime}GUILDINFO.json`, JSON.stringify(translateGuilds), async (err) => {
       if (err){
         console.log(err)
