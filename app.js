51a8894a0f37ba75733419cabaaf8d6ee1a1e40a
sonawaneanand96 <sonawaneanand95@gmail.com>
2018-06-24 17:47:31 +0530

    Update app.js

--- app.js
+++ app.js
@@ -43,7 +43,7 @@ bot.on("ready", () => {
 bot.on("messageCreate", async msg => {
   if(msg.author.bot) return
   if(msg.channel.guild.id === MyServer.guild){
-     if(msg.channel.id === MyServer.general){bot.createMessage(MyServer2.general, `**${msg.author.username}**: ${msg.content} ${msg.attachments.url}`)}
+     if(msg.channel.id === MyServer.general){bot.createMessage(MyServer2.general, `**${msg.author.username}**: ${msg.content} ${msg.attachments[0].url}`)}
      if(msg.channel.id === MyServer.englishinfo){bot.createMessage(MyServer2.englishinfo, `**${msg.author.username}**: ${msg.content} ${msg.attachments[0]}`)}
   }
   const tsChannelsEnabled = tlcfg.tsChannelsEnabled
